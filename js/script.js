365f7f8460657818917a5cf6b86871dd42593d98
Kamil Trzcinski <asasn233@gmail.com>
2023-11-26 14:09:29 +0100

    finished function titleClickHandler

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
 'use strict';
 
 function titleClickHandler(event){
+  event.preventDefault();
   const clickedElement = this;
   console.log('Link was clicked!');
-
   /* remove class 'active' from all article links  */
 
   const activeLinks = document.querySelectorAll('.titles a.active');
@@ -20,7 +20,7 @@ console.log('clickedElement (with plus): ' + clickedElement);
 
   /* remove class 'active' from all articles */
 
-  const activeArticles = document.querySelectorAll('.posts a.active');
+  const activeArticles = document.querySelectorAll('.posts article.active');
 
   for(let activeArticle of activeArticles){
     activeArticle.classList.remove('active');
@@ -28,9 +28,18 @@ console.log('clickedElement (with plus): ' + clickedElement);
 
   /* get 'href' attribute from the clicked link */
 
+  const articleSelector = clickedElement.getAttribute("href");
+  console.log(articleSelector);
+
   /* find the correct article using the selector (value of 'href' attribute) */
 
+  const targetArticle = document.querySelector(articleSelector);
+  console.log(targetArticle);
+
   /* add class 'active' to the correct article */
+
+  targetArticle.classList.add('active');
+
 }
 
 const links = document.querySelectorAll('.titles a');
