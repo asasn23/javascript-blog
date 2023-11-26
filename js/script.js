c03dfbf856b47b199b9835065a0558294778b83d
Kamil Trzcinski <asasn233@gmail.com>
2023-11-26 13:48:47 +0100

    add clickedElement this & active

--- js/script.js
+++ js/script.js
@@ -1,14 +1,30 @@
 'use strict';
 
 function titleClickHandler(event){
+  const clickedElement = this;
   console.log('Link was clicked!');
-  console.log(event);
 
   /* remove class 'active' from all article links  */
 
+  const activeLinks = document.querySelectorAll('.titles a.active');
+
+for(let activeLink of activeLinks){
+  activeLink.classList.remove('active');
+}
+
   /* add class 'active' to the clicked link */
+  
+clickedElement.classList.add('active');
+console.log('clickedElement:', clickedElement);
+console.log('clickedElement (with plus): ' + clickedElement);
 
   /* remove class 'active' from all articles */
+
+  const activeArticles = document.querySelectorAll('.posts a.active');
+
+  for(let activeArticle of activeArticles){
+    activeArticle.classList.remove('active');
+  }
 
   /* get 'href' attribute from the clicked link */
 
