a4d5187c4322b1fdd45e82128a300f000ef0c00e
Kamil Trzcinski <asasn233@gmail.com>
2023-11-27 12:47:39 +0100

    add function generateTitleLinks and fixed bug

--- js/script.js
+++ js/script.js
@@ -42,8 +42,50 @@ console.log('clickedElement (with plus): ' + clickedElement);
 
 }
 
-const links = document.querySelectorAll('.titles a');
 
-for(let link of links){
-  link.addEventListener('click', titleClickHandler);
+
+
+const optArticleSelector = '.post',
+  optTitleSelector = '.post-title',
+  optTitleListSelector = '.titles';
+
+function generateTitleLinks(){
+
+  /* remove contents of titleList */
+
+  const titleList = document.querySelector(optTitleListSelector);
+  titleList.innerHTML = '';
+
+
+  /* for each article */
+
+  const articles = document.querySelectorAll('.post');
+
+  let html = '';
+
+for(let article of articles){
+  /* get the article id */
+  const articleId = article.getAttribute("id");
+  /* find the title element */
+  const articleTitle = article.querySelector(optTitleSelector).innerHTML;
+  /* create HTML of the link */
+  const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
+  /* get the title from the title element */
+  html = html + linkHTML
+  console.log(html);
 }
+
+titleList.innerHTML = html;
+
+    /* insert link into titleList */
+
+    const links = document.querySelectorAll('.titles a');
+    console.log(links);
+    
+    for(let link of links){
+      link.addEventListener('click', titleClickHandler);
+    }
+
+}
+
+generateTitleLinks();
